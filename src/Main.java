0656ea49430d3d60384ec11445f540a76b969d87
Allectona <miranos.hill@gmail.com>
2023-08-14 13:35:22 +0100

    Initial Commit (1 and 2 exercises)

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* 1 задание */
+
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " ,то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " ,то он не достиг совершеннолетия, нужно немного подождать");
+                    }
+
+        /* 2 задание */
+
+        int temperature = 1;
+        if (temperature >=5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки!");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку!");
+        }
+    }
+}
